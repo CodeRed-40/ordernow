ca1b6f32b2ef695b625b4f790ed66a45c04db333
stillflow <72228870+stillflow@users.noreply.github.com>
2024-12-23 18:49:24 -0500

    Initial commit

---
+++ src/main/java/com/myapp/ordernow/OrdernowApplication.java
@@ -0,0 +1,13 @@
+package com.myapp.ordernow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrdernowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrdernowApplication.class, args);
+	}
+
+}
