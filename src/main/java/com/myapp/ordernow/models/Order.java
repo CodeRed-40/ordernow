4a78eba3c680fa75eb7dcfc87aece5bfc8a04565
oledibefrancis <somtofrancis380@gmail.com>
2024-12-26 20:33:08 -0800

    use @data annotation and get rid of getters & setters

--- src/main/java/com/myapp/ordernow/models/Order.java
+++ src/main/java/com/myapp/ordernow/models/Order.java
@@ -2,10 +2,12 @@ package com.myapp.ordernow.models;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
+import lombok.Data;
 
 @Entity
 @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 @Table(name = "Orders")
+@Data
 public class Order {
     private String name;
     private String address;
@@ -14,36 +16,4 @@ public class Order {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private Integer id;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public Integer getId() {
-        return id;
-    }
 }
